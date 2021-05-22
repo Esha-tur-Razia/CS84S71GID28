602181a3581624ddfeadff8325786acd4a1e0241
Esha-tur-Razia <81893933+Esha-tur-Razia@users.noreply.github.com>
2021-05-22 11:26:41 -0700

    Added a menu for homepage
    updated form1

--- recipeManagement/recipeManagement/Form1.Designer.cs
+++ recipeManagement/recipeManagement/Form1.Designer.cs
@@ -31,6 +31,16 @@ namespace recipeManagement
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homepageToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.goToHomepageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -47,20 +57,88 @@ namespace recipeManagement
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Location = new System.Drawing.Point(-1, 1);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(210, 751);
             this.panel1.TabIndex = 1;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(209, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(841, 43);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.MenuText;
+            this.panel3.Location = new System.Drawing.Point(207, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(909, 79);
+            this.panel3.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homepageToolStripMenuItem1,
+            this.aboutUsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(840, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homepageToolStripMenuItem1
+            // 
+            this.homepageToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goToHomepageToolStripMenuItem});
+            this.homepageToolStripMenuItem1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.homepageToolStripMenuItem1.Name = "homepageToolStripMenuItem1";
+            this.homepageToolStripMenuItem1.Size = new System.Drawing.Size(78, 20);
+            this.homepageToolStripMenuItem1.Text = "Homepage";
+            // 
+            // goToHomepageToolStripMenuItem
+            // 
+            this.goToHomepageToolStripMenuItem.Name = "goToHomepageToolStripMenuItem";
+            this.goToHomepageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.goToHomepageToolStripMenuItem.Text = "Go to homepage";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            this.aboutUsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contactUsToolStripMenuItem});
+            this.aboutUsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.aboutUsToolStripMenuItem.Text = "about us";
+            // 
+            // contactUsToolStripMenuItem
+            // 
+            this.contactUsToolStripMenuItem.Name = "contactUsToolStripMenuItem";
+            this.contactUsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.contactUsToolStripMenuItem.Text = "contact us";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(840, 427);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +148,13 @@ namespace recipeManagement
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homepageToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem goToHomepageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactUsToolStripMenuItem;
     }
 }
 
