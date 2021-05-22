364e1505a51bfe93afade5cf6ea419d8945d352f
Esha-tur-Razia <81893933+Esha-tur-Razia@users.noreply.github.com>
2021-05-22 11:03:49 -0700

    updated form 1
    new panel added

--- recipeManagement/recipeManagement/Form1.Designer.cs
+++ recipeManagement/recipeManagement/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace recipeManagement
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -43,11 +44,20 @@ namespace recipeManagement
             this.label1.TabIndex = 0;
             this.label1.Text = "Recipe management\r\nSystem";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Location = new System.Drawing.Point(-1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(210, 751);
+            this.panel1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(840, 427);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +69,7 @@ namespace recipeManagement
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
