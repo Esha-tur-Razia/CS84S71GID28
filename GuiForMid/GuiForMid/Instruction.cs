145cbcb9630628feb3621cfad13dd2bc2992b66d
Esha-tur-Razia <81893933+Esha-tur-Razia@users.noreply.github.com>
2021-05-31 01:36:52 -0700

    updated instruction.cs
    Added constructor and to string() method + attributes added

--- GuiForMid/GuiForMid/Instruction.cs
+++ GuiForMid/GuiForMid/Instruction.cs
@@ -8,5 +8,39 @@ namespace GuiForMid
 {
     class Instruction
     {
+        private String cookTime;
+        private String temprature;
+        private String narrative;
+
+
+
+        public Instruction()
+        {
+        }
+
+        public Instruction(string cookTime, string temprature, string narrative)
+        {
+            this.cookTime = cookTime;
+            this.temprature = temprature;
+            this.narrative = narrative;
+        }
+
+
+
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
