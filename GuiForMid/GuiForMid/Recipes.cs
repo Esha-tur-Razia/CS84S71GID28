d66da3e60f8274fc11643e821c37e7d73eb8f851
Esha-tur-Razia <81893933+Esha-tur-Razia@users.noreply.github.com>
2021-05-31 01:20:15 -0700

    Updated Recipes.cs
    Added attribute getter ,setter ,constructor ,to string()  and equals()

--- GuiForMid/GuiForMid/Recipes.cs
+++ GuiForMid/GuiForMid/Recipes.cs
@@ -8,5 +8,33 @@ namespace GuiForMid
 {
     class Recipes
     {
+        private string Title;
+        private Instruction instruction;
+
+        public Recipes(string title, Instruction instruction)
+        {
+            Title = title;
+            this.instruction = instruction;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Recipes recipes &&
+                   Title == recipes.Title &&
+                   EqualityComparer<Instruction>.Default.Equals(instruction, recipes.instruction);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1631324304;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Title);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Instruction>.Default.GetHashCode(instruction);
+            return hashCode;
+        }
     }
 }
