0a3a3bb37335dc29225f5efe2e6b630e785a4306
SanaShafique <shafique.sana321@gmail.com>
2021-06-18 00:16:35 -0700

    add recipie
    add button

--- GuiForMid/GuiForMid/add recipie.cs
+++ GuiForMid/GuiForMid/add recipie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,17 @@ namespace GuiForMid
 {
     public partial class add_recipie : Form
     {
+      
+
         public add_recipie()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+           
         }
     }
 }
